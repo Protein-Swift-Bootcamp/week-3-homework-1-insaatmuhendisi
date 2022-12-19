0473531558f318ab8ad1db174d114febec699117
Halil Ibrahim Andic <hiandic@gmail.com>
2022-12-19 17:05:36 +0300

    initial settings are applied
    2nd VC is created and connected with 2nd screen in storyboard. Additional initial settings in .plist and other files are applied.

---
+++ simpra_hw1_halil/simpra_hw1_halil/Controllers/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  simpra_hw1_halil
+//
+//  Created by Halil Ibrahim Andic on 19.12.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ simpra_hw1_halil/simpra_hw1_halil/Controllers/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  simpra_hw1_halil
+//
+//  Created by Halil Ibrahim Andic on 19.12.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+}

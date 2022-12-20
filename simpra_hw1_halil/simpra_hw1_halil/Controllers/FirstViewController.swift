344b8a8a7b443300d53ebcdc9198d8db1336ec14
Halil Ibrahim Andic <hiandic@gmail.com>
2022-12-20 18:47:50 +0300

    Notification Center is completed

--- simpra_hw1_halil/simpra_hw1_halil/Controllers/FirstViewController.swift
+++ simpra_hw1_halil/simpra_hw1_halil/Controllers/FirstViewController.swift
@@ -10,27 +10,45 @@ import UIKit
 //MARK: - DELEGATE
 class FirstViewController: UIViewController {
 
+    
     @IBOutlet weak var dataLabel: UILabel!
     @IBOutlet weak var transferLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataLabel.text = ""
-        transferLabel.text = ""
+        
+        // create an instance of 2nd VC
+        let secondVC = SecondViewController()
+        
+        // tells 2nd VC that i am the current delegate
+        secondVC.delegate = self
+        
+        // register to receive notification
+        NotificationCenter.default.addObserver(self, selector: #selector(didNotificationArrive(notification:)), name: NSNotification.Name(rawValue: "notificationName"), object: nil)
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        //present(secondVC, animated: true, completion: nil)
         self.performSegue(withIdentifier: "goToSecond", sender: self)
     }
-    
-
 }
 
 //MARK: - DELEGATE EXTENSION
 extension FirstViewController: canTransfer {
-    
-    func transfer(data: String) {
+
+    // protocol function
+    func didTransfer(_ data: String) {
         print(data)
+        dataLabel.text = data
     }
+    
+    // notification center observer (handles the incoming notification)
+    @objc func didNotificationArrive(notification: NSNotification) {
+        if let data = notification.userInfo?["entry"] as? String {
+            dataLabel.text = data
+            transferLabel.text = "transfered by notification center"
+        }
+    }
+    
 }
--- simpra_hw1_halil/simpra_hw1_halil/Controllers/SecondViewController.swift
+++ simpra_hw1_halil/simpra_hw1_halil/Controllers/SecondViewController.swift
@@ -8,46 +8,58 @@ import UIKit
 
 //MARK: - PROTOCOL
 protocol canTransfer {
-    func transfer(data: String)
+    func didTransfer(_ data: String)
 }
 
 //MARK: - DELEGATOR
 class SecondViewController: UIViewController {
     
-    
     @IBOutlet weak var textField: UITextField!
     
+    let data = "hi"
     var delegate: canTransfer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
-    func toDelegation() {
-        delegate?.transfer(data: "hi")
+    // this function reads the data written and checks if it is nil
+    func gatherData() -> String {
+        guard let entry = textField.text else {
+            return "Nothing written"
+        }
+        return entry
     }
     
+    // triggers when protocol button pressed
     @IBAction func protocolButtonPressed(_ sender: UIButton) {
-        
-        
-        //go back to the 1st screen
+        delegate?.didTransfer(data)
+  
         self.dismiss(animated: true, completion: nil)
     }
     
+    
+    // triggers when NC button pressed
     @IBAction func NCButtonPressed(_ sender: UIButton) {
+        // create data dictionary
+        let dataDict = ["entry" : gatherData()]
         
+        // post a notification
+        NotificationCenter.default.post(name: NSNotification.Name("notificationName"), object: nil, userInfo: dataDict)
         
         //go back to the 1st screen
         self.dismiss(animated: true, completion: nil)
     }
     
+    // triggers when closure button pressed
     @IBAction func closureButtonPressed(_ sender: UIButton) {
         
         
         //go back to the 1st screen
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
 }
+
+//MARK: - EXTENSION
+
